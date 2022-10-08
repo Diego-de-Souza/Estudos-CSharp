a246f81c7255a6b3cc1a353d4897f4972f235cab
DIEGO DE SOUZA LIMA <082220042@faculdade.cefsa.edu.br>
2022-10-07 22:39:05 -0300

    nova pasta, como exercicio do for

---
+++ exercicios/Aula_07102022_laçoFOR/Aula_07102022_laçoFOR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_07102022_laçoFOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int cont=1; cont <=10; cont++)
+            {
+                Console.WriteLine($"Numero: {cont}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
