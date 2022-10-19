370c6d11106251ea7bd0fec7311ac104135cdeb1
DIEGO DE SOUZA LIMA <082220042@faculdade.cefsa.edu.br>
2022-10-19 20:50:32 -0300

    Exercicio 42 da lista

---
+++ exercicios/EX42_lista de exercicio/EX42_lista de exercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX42_lista_de_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //solicita informação do usuário
+            Console.Write("Digite o código do detento: ");
+            int codDetento = Console.ReadLine();
+
+            Console.Write("Digite o nome do Detento: ");
+            string nomeDetento = Console.ReadLine();
+
+            Console.Write("Digite condenação do Detento: ");
+            string condenacao = Console.ReadLine();
+
+            Console.Write("Digite a pena do Detento: ");
+            Int32 pena = Console.ReadLine();
+
+        }
+    }
+}
