05e85ccbc68b7823d996a2bd7183f74586aae4bf
Diego-de-Souza <49752675+Diego-de-Souza@users.noreply.github.com>
2023-02-28 14:17:40 -0300

    Criação de uma nova classe
    Criação de uma nova classe com C# no visual studio.

---
+++ estudos/classes/Classes_CSharp/Classes_CSharp/Cadastro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_CSharp
+{
+    public class Cadastro
+    {
+        public Int32 cod;
+        public string nome;
+        public string sobreNome;
+        public string eMail;
+
+        public void nomeCompleto(){
+           string nomeFUll = this.nome + ' ' + this.sobreNome;
+        }
+    }
+}
