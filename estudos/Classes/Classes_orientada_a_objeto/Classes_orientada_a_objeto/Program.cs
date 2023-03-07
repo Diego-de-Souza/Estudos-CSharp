c42334c5c2c6f744b8ddc53d356b0b367cc50f69
DIEGO DE SOUZA LIMA <082220042@faculdade.cefsa.edu.br>
2023-03-07 20:51:37 -0300

    classes

---
+++ estudos/Classes/Classes_orientada_a_objeto/Classes_orientada_a_objeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_orientada_a_objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
