960d22b1a6ce23010bf218e8424e70efb172b6fc
DIEGO DE SOUZA LIMA <082220042@faculdade.cefsa.edu.br>
2023-03-13 20:53:47 -0300

    exercicios 13 e 14/03

---
+++ exercicios_2S/Calcular_media_final_aluno/Calcular_media_final_aluno/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_media_final_aluno
+{
+    internal class Aluno
+    {
+        public string nome;
+        public int idade;
+        public double mediaFinal;
+
+        public Aluno(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public void calcularMediaFinal(double media1, double media2)
+        {
+            this.mediaFinal = (media1 + media2) / 2;
+        }
+
+        public void Aprovar()
+        {
+            if(this.mediaFinal >= 7)
+            {
+                string Aprovacao = "Aprovado";
+            }else
+            {
+                string Aprovacao = "Reprovado";
+            }
+        }
+
+    }
+}
---
+++ exercicios_2S/Calcular_media_final_aluno/Calcular_media_final_aluno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_media_final_aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequencia = "";
+            string[] alunos = new
+            do { 
+            Console.WriteLine("Digite o nome do Aluno: ")
+            
+            }while(sequencia == "Y" || sequencia == "y"){
+
+            }
+        }
+    }
+}
