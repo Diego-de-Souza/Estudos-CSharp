9850448b7e6cce3a14454da81fd7532a08a7495d
Diego-de-Souza <49752675+Diego-de-Souza@users.noreply.github.com>
2023-03-21 17:20:32 -0300

    exercicio 1 e 2 algoritmo
    exercicio 1 e 2 de algoritmo para composição da N1 do segundo bi.

---
+++ exercicios_2S/exercicio_2_14032023/exercicio_2_14032023/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_2_14032023
+{
+    internal class ContaBancaria
+    {
+        public string nomeTitular;
+        public int numeroConta;
+        public double saldo;
+        public string statusOperacao;
+
+
+        public void depositar(double deposito)
+        {
+            this.saldo += deposito;
+        }
+
+        public void Sacar(double retirada)
+        {
+            if(retirada < this.saldo)
+            {
+                this.saldo -= retirada;
+                this.statusOperacao = "true";
+            }else
+            {
+                this.statusOperacao = "false";
+            }
+        }
+    }
+}
---
+++ exercicios_2S/exercicio_2_14032023/exercicio_2_14032023/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_2_14032023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicia um objeto vetor para guardar sa informações do aluno
+            ContaBancaria[] contaBanco = new ContaBancaria[3];
+            //gerando um objeto de numero randomico
+            Random rnd = new Random();
+
+            for (int dep = 0; dep <= 2; dep++)
+            {
+                contaBanco[dep].depositar(rnd.Next(1, 1000));
+                contaBanco[dep].nomeTitular = "Cliente " + dep;
+                contaBanco[dep].numeroConta = 000020320 + dep;
+            }
+
+            do
+            {
+                
+            } while (contaBanco[0].saldo > 0 || contaBanco[1].saldo > 0 || contaBanco[2].saldo > 0);
+        }
+    }
+}
