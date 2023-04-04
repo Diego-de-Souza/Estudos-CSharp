39851176c1de74ef1a55cd9475d284081cec3c5c
DIEGO DE SOUZA LIMA <082220042@faculdade.cefsa.edu.br>
2023-04-04 20:53:22 -0300

    atividade

---
+++ exercicios_2S/exercicio_04042023/Atividade_sistema_biblioteca_20230404/Atividade_sistema_biblioteca_20230404/Livro1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_sistema_biblioteca_20230404
+{
+    internal class Livro
+    {
+        /// <summary>
+        /// Classe livro criada para atividade de desenvolvimento de biblioteca
+        /// </summary>
+        
+        //declaração de atributos
+        public string titulo;
+        public string autor;
+        public bool disponibilidade;
+
+        public Livro(string titulo, string autor)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+        }
+
+        //metodo que identifica se o livro está ou não emprestado
+        public void Emprestar(string emprestimo)
+        {
+            if(emprestimo == "esprestado")
+            {
+                this.disponibilidade = true;
+            }else
+            {
+                this.disponibilidade = false;
+            }
+        }
+
+        //metodo que identifica de o livro está ou não devolvido
+        public void Devolver(string devolucao)
+        {
+            if(devolucao == "devolvido")
+            {
+                this.disponibilidade = true;
+            } else
+            {
+                this.disponibilidade= false;
+            }
+        }
+
+        //metodo que chama as informações de mostra para o usuário
+        public void MostrarInformacoes(string informacoes)
+        {
+
+        }
+
+    }
+}
---
+++ exercicios_2S/exercicio_04042023/Atividade_sistema_biblioteca_20230404/Atividade_sistema_biblioteca_20230404/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_sistema_biblioteca_20230404
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Este programa é um sistema de Biblioteca, tem como função
+            /// o controle dos livros da biblioteca.
+            /// </summary>
+
+            //declara as variáveis de entrada
+            string nomeLivro, autorLivro, contInsercaoLivro="S";
+            Livro[] listaDeLivros = new Livro[100];
+
+
+            //solicita ao usuário o nome do livro a ser procurado
+            while(contInsercaoLivro == "S") { 
+            Console.WriteLine("Digite o nome do livro ");
+            nomeLivro = Console.ReadLine();
+            Console.WriteLine("Qual o autor do livro? ");
+            autorLivro = Console.ReadLine();
+
+            //insere as informações na lista
+            livrosBiblioteca.Add(new Livro(nomeLivro, autorLivro));
+            
+            Console.WriteLine("Deseja cadastrar outro livro? (S/N)");
+            contInsercaoLivro = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine(livrosBiblioteca.);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
